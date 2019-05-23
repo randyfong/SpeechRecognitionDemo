36926424afa95179a1c6b929671ba926dd5b600d
Randy Fong <randyfong@aol.com>
2019-05-23 13:33:21 -0600

    Initial Commit

---
+++ SpeechRecognitionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechRecognitionDemo
+//
+//  Created by Randy Fong on 5/23/19.
+//  Copyright © 2019 Randall Fong Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
